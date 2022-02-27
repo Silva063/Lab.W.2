7ff2ccc479726c7a29a06e9c35140f76fa135672
Silva063 <slavaratu32@gmail.com>
2022-02-27 17:49:54 +0200

    First Commit

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab.W._2._1._13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double a = 17.421;
+            double b = 10.365 * Math.Pow(10, -3);
+            double c = 0.828 * Math.Pow(10, 5);
+            textBox1.Text = a.ToString();
+            textBox2.Text = b.ToString();
+            textBox3.Text = c.ToString();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            textBox4.Text += Environment.NewLine+"X=" + x.ToString();
+
+            double y = double.Parse(textBox2.Text);
+            textBox4.Text += Environment.NewLine + "Y=" + y.ToString();
+
+            double z = double.Parse(textBox3.Text);
+            textBox4.Text += Environment.NewLine + "Z=" + z.ToString();
+
+            double f = (Math.Pow((y + Math.Pow(x - 1d, 1d / 3d)), 1d / 4d)) / (Math.Abs(x - y) * (Math.Pow(Math.Sin(z), 2) + Math.Tan(z)));
+
+            textBox4.Text += Environment.NewLine +"Результат F =" + f.ToString();    
+        }
+    }
+}
